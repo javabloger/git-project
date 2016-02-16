876d29a976d435b26fa03f73c9da8feed1813805
javabloger <javabloger@163.com>
2016-02-16 14:46:37 +0800

    修改代码

--- JavaClass.java
+++ JavaClass.java
@@ -5,7 +5,7 @@ public class JavaClass {
 
 	public static void main (String [] args){
 
-		System.out.println("test print ~! ");
-
+		// System.out.println("test print ~! ");
+	  System.out.println("hello world  ~! ");
 	}
 }
